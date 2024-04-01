6a61de0f9e76e21c44ce115713496599a7c55fe1
frykhamn <gabriella.frykhamn@sensera.se>
2024-04-01 14:00:14 +0200

    about done

--- src/pages/about-page/AboutPage.jsx
+++ src/pages/about-page/AboutPage.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { InfoSection } from './components/InfoSection';
+import { CVSection } from './components/CVSection';
 
 const AboutPage = () => {
     return (
-        <section className="conatiner mx-auto pb-20 pt-32 md:w-4/5 xl:max-w-[1000px]">
-      <InfoSection></InfoSection>
+        <><section className="conatiner mx-auto pb-20 pt-32 md:w-4/5 xl:max-w-[1000px]">
+        <InfoSection></InfoSection>
       </section>
-
+      <section className='bg-textAcc pb-20 pt-32'>
+        <CVSection></CVSection>
+        </section></>
     );
 };
 
---
+++ src/pages/about-page/components/CVSection.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import circle from '../../../assets/gold-circle.png'
+
+export const CVSection = () => {
+    return (
+        <div className="conatiner mx-auto pb-20 pt-22 md:w-4/5 xl:max-w-[1000px]">
+
+            <div className="grid grid-cols-2 gap-10">
+                <div className="  text-textOffLight text-4xl  text-center font-cor place-self-center">
+                    <h2 className='w-64'>The theraputhic converastion, to investigate and unveil</h2>
+                </div>
+                <div className=" text-textOffLight text-4xl  font-cor place-self-center">
+                </div>
+                <div className=' place-self-center w-60'>
+                    <img src={circle} alt="circle" className='w-full' />
+                </div>
+                <div className=' place-self-start md:w-[24rem] font-cor text-lg' >
+                    <div className='flex text-textOffLight list-none gap-2'>
+                        <li>Sweden - </li>
+                        <li> India - </li>
+                        <li> Kambodya - </li>
+                        <li> Mexico - </li>
+                        <li> Online</li>
+                    </div>
+                    <h2 className='text-white text-3xl pb-4 font-semibold tracking-wide'>Body treatments</h2>
+                    <div className='text-textOffLight flex gap-4 flex-col'>
+                        <p>
+                            Isabella is the creator of Cruma Recover and has, in collaboration with Curam's founder Marie Jansson (link)
+                        </p>
+                        <p>
+                            Four-star hotel spa concept worldwide ResonDetre (link)
+                        </p>
+                        <p>
+                            Creator of Grand Hotel's spa concept (link)
+                        </p>
+                    </div>
+                </div>
+                <div className='pt-10 text-white font-cor text-4xl col-span-2 text-center'>
+                    <p>
+                    "When someone says they received a poor treatment, I'm always curious: Did you express what you wanted? No, is often the answer."
+                    </p>
+                </div>
+            </div>
+
+        </div>
+    )
+}
